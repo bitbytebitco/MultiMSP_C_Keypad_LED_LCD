8366ab9c40c21561791ccd20e5144ecb007daeaa
bitbytebitco <45221627+bitbytebitco@users.noreply.github.com>
2023-03-19 18:03:30 -0600

    Update LCD_slave.c
    I2C receive now working with entering a character, using while loop (since sendByte action too long for inside interrupt)

--- LCD_slave.c
+++ LCD_slave.c
@@ -1,15 +1,13 @@
 #include <msp430.h> 
 
-volatile int Rx_Command = 0;
-
-volatile int timer_action_select;
+int Rx_Command = 0;
+volatile int action_select = 0;
 volatile int ms_thresh, ms_count, ms_flag;
-//char rx_packet = {0x00};
 
 void initI2C_slave(){
     UCB0CTLW0 |= UCSWRST;       // SW RESET enabled
     UCB0CTLW0 |= UCMODE_3 | UCSYNC;      // Put into I2C mode
-    UCB0I2COA0 = 0x0058 | UCOAEN; // Set slave address + enable
+    UCB0I2COA0 = 0x0068 | UCOAEN; // Set slave address + enable
 
     // setup ports
     P1SEL1 &= ~BIT3;            // P1.3 SCL
@@ -18,7 +16,6 @@ void initI2C_slave(){
     P1SEL1 &= ~BIT2;            // P1.2 SDA
     P1SEL0 |= BIT2;
 
-    PM5CTL0 &= ~LOCKLPM5;       // turn on I/O
     UCB0CTLW0 &= ~UCSWRST;      // SW RESET OFF
 
     UCB0IE |= UCTXIE0 | UCRXIE0 | UCSTPIE;          // enable I2C B0 Tx/Rx/Stop IRQs
@@ -50,15 +47,15 @@ void init(){
     P2DIR |= BIT6;  // set P2.6 output for E (LCD), data starts on falling edge
     P2OUT &= ~BIT6; // clear P2.6
 
-    P2DIR |= BIT0;  // P1.1 LED output
-    P2OUT &= ~BIT0; // clear P1.1
+    P1DIR |= BIT1;  // P1.1 LED output
+    P1OUT &= ~BIT1; // clear P1.1
 
-    initTimerB0compare();
-    initTimerB1compare();
     initI2C_slave();
+    initTimerB0compare();
 
     __enable_interrupt();
 }
+
 
 void delay1000(){
     int i;
@@ -84,7 +81,7 @@ void latch(){
 }
 
 void delay_ms(int ms){
-    P2OUT |= BIT0; // LED on
+    P1OUT |= BIT1; // LED on
     ms_flag = 0;
     ms_count = 0;
     ms_thresh = ms;
@@ -92,64 +89,11 @@ void delay_ms(int ms){
     while(ms_flag != 1){}
     TB0CCTL0 &= ~CCIE;       // disable CCR0
     //flag_30_ms = 0;
-    P2OUT &= ~BIT0; // LED off
+    P1OUT &= ~BIT1; // LED off
 }
 
 
-void LCDsetup() {
 
-    configure();
-    latch();
-    delay_ms(10);
-
-    configure();
-    latch();
-    delay_ms(10);
-
-    configure();
-    latch();
-    delay_ms(10);
-
-    configure2(); // funcset interface 4 bit
-    latch();
-    delay_ms(10);
-
-
-    // funcset interface 4 bit
-//    P2OUT &= ~BIT7; // clear RS
-//    P1OUT &= ~(BIT7 | BIT6 | BIT4);
-//    P1OUT |= (BIT5);
-//    latch();
-//    delay_ms(10);
-
-    sendByte(0b00101100, 0); // function set interface 4-bit & spec display lines and fonts
-
-    sendByte(0b00001000, 0); // display off
-
-    sendByte(0b00000001, 0); // clear display
-
-    // spec display lines and fonts
-//    P2OUT &= ~BIT7; // clear RS
-//    P1OUT |= (BIT6);
-//    P1OUT &= ~(BIT7 | BIT5 | BIT4);
-//    latch();
-//    delay_ms(10);
-
-    // display on/off
-//    P2OUT &= ~BIT7; // clear RS
-//    P1OUT &= ~(BIT7 | BIT6 | BIT5 | BIT4);
-//    latch();
-//    delay_ms(10);
-//    P2OUT &= ~BIT7; // clear RS
-//    P1OUT |= (BIT7 | BIT6 );
-//    P1OUT &= ~(BIT5 | BIT4);
-//
-//    latch();
-//    delay_ms(30);
-
-    sendByte(0b00000110, 0); // entry mode set
-
-}
 
 void clear_display(){
     P2OUT &= ~BIT7; // set RS
@@ -217,7 +161,7 @@ void sendNibble(unsigned short byte, int rs) {
         n = n << 1;
     }
     latch();
-    delay_ms(10);
+    delay_ms(2);
 }
 
 void sendByte(int char_code, int rs){
@@ -227,69 +171,74 @@ void sendByte(int char_code, int rs){
 
 void incr_index_reg_right(){
     sendByte(0b00010100, 0);
-
 }
 
-void executeCommand(int command){
-    switch(command){
-        case 0x80:
-            // Character "C" as a test
-                sendByte(0b01000001, 1);
-            break;
-        case 0x40:
-            // Character "C" as a test
-                sendByte(0b01000010, 1);
-            break;
-    }
+void LCDsetup() {
+
+    configure();
+    latch();
+    delay_ms(10);
+
+    configure();
+    latch();
+    delay_ms(10);
+
+    configure();
+    latch();
+    delay_ms(10);
+
+    configure2(); // funcset interface 4 bit
+    latch();
+    delay_ms(10);
+
+    sendByte(0b00101100, 0); // function set interface 4-bit & spec display lines and fonts
+
+    sendByte(0b00001000, 0); // display off
+
+    sendByte(0b00000001, 0); // clear display
+
+    sendByte(0b00000110, 0); // entry mode set
+
 }
-
-void initTimerB1compare(){
-    // setup TB0
-    TB1CTL |= TBCLR;        // Clear TB0
-    TB1CTL |= TBSSEL__ACLK; // select SMCLK
-    TB1CTL |= MC__UP;       // UP mode
-    TB1CCTL0 |= CCIE;       // local IRQ enable for CCR0
-
-    //TB0CCTL0 &= ~CCIFG;     // clear CCR0 flag
-}
-
 
 int main(void)
 {
     init();
-    //P1OUT ^= BIT1; // clear P1.1
-    //delay_ms(20);
-//    LCDsetup();
-//
-//    sendByte(0b00001111, 0); // display on
-//
-//    sendByte(0b01000001, 1);
-//    delay_ms(1);
-//    sendByte(0b01000001, 1);
-//    delay_ms(1);
-//    sendByte(0b01000001, 1);
-//    delay_ms(1);
-//    sendByte(0b01000001, 1);
+    delay_ms(20);
+    LCDsetup();
 
-    while(1){}
+    sendByte(0b00001111, 0); // display on
+    clear_display();
+
+//    sendByte(0b01000001, 1); // "A"
+
+    while(1){
+        if(action_select == 1){
+            sendByte(0b01000001, 1); // "A"
+            action_select = 0;
+        }
+    }
 
     return 0;
 }
 
+void executeCommand(int command){
+}
 
 #pragma vector=EUSCI_B0_VECTOR
 __interrupt void EUSCI_B0_TX_ISR(void){
-    Rx_Command = UCB0RXBUF;    // Retrieve byte from buffer
+    switch(UCB0IV){
+        case 0x16:  // receiving
+                Rx_Command = UCB0RXBUF;    // Retrieve byte from buffer
+//                executeCommand(Rx_Command);
+                action_select = 1;
+                P1OUT ^= BIT1;
+            break;
+        case 0x18:
+            break;
+    }
 
-//    P2OUT ^= BIT0; // clear P1.1
-//    if(Rx_Command == 0x80){
-        TB1CCTL0 |= CCIE;       // local IRQ enable for CCR0
-        TB1CCR0 = 2000;         // set CCR0 value (period)
-        TB1CCTL0 &= ~CCIFG;     // clear CCR0 flag to begin count
-//    }
-
-
-//    UCB0IFG &= ~UCTXIFG0;   // clear flag to allow I2C interrupt
+    UCB0IFG &= ~UCTXIFG0;   // clear flag to allow I2C interrupt
 }
 
 #pragma vector=TIMER0_B0_VECTOR
@@ -302,15 +251,3 @@ __interrupt void ISR_TB0_CCR0(void){
 
     TB0CCTL0 &= ~CCIFG;
 }
-
-#pragma vector=TIMER1_B0_VECTOR
-__interrupt void ISR_TB1_CCR0(void){
-    //executeCommand(Rx_Command);
-
-    P2OUT ^= BIT0; // clear P1.1
-    TB1CCTL0 &= ~CCIE; // disable timer
-    TB1CCTL0 |= CCIFG;
-
-    UCB0IFG &= ~UCTXIFG0;   // clear flag to allow I2C interrupt
-}
-
